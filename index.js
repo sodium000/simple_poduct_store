0ce2b378566a776b178a296a044caffd58123d66
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-11-04 23:16:33 +0600

    for home

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -26,63 +26,91 @@ const client = new MongoClient(uri, {
   },
 });
 
+async function run() {
+  try {
+    await client.connect();
 
-async function run(){
-    try {
-        await client.connect();
+    const dataBase = client.db("samrt-porduct");
+    const productCollection = dataBase.collection("porducts");
+    const bitsCollection = dataBase.collection("bids");
+    const UserCollection = dataBase.collection("Users");
 
-        const dataBase = client.db("samrt-porduct")
-        const productCollection = dataBase.collection("porducts");
+    // users api
 
-        app.post("/porducts",async (req,res)=>{
-            const newProduect = req.body;
-            console.log(newProduect);
-            const resuslt = await productCollection.insertOne(newProduect)
-            res.send(resuslt);
-        })
+    app.post("/users", async (req,res) => {
+      const Newusers = req.body;
+      const UserEmail = req.body.email;
+      const query = { email: UserEmail };
+      const existingUser = await UserCollection.findOne(query);
+      if (existingUser) {
+        res.send({ message: "user already exist" });
+      } else {
+        const resuslt = await UserCollection.insertOne(Newusers);
+        res.send(resuslt);
+      }
+    });
 
+    app.post("/porducts", async (req, res) => {
+      const newProduect = req.body;
+      console.log(newProduect);
+      const resuslt = await productCollection.insertOne(newProduect);
+      res.send(resuslt);
+    });
 
-        app.delete('/porducts/:id', async (req,res) => {
-            const ID = req.params.id
-            const query = {
-                "_id" : new ObjectId(ID)
-            }
-            const resuslt = await productCollection.deleteOne(query)
-            res.send(resuslt);
+    app.delete("/porducts/:id", async (req, res) => {
+      const ID = req.params.id;
+      const query = {
+        _id: new ObjectId(ID),
+      };
+      const resuslt = await productCollection.deleteOne(query);
+      res.send(resuslt);
+    });
 
-        })
+    app.patch("/porducts/:id", async (req, res) => {
+      const ID = req.params.id;
+      const updateProduce = req.body;
+      const query = {
+        _id: new ObjectId(ID),
+      };
+      const update = {
+        $set: { name: updateProduce.name, Price: updateProduce.Price },
+      };
+      const option = {};
+      const resuslt = await productCollection.updateOne(query, update, option);
+      res.send(resuslt);
+    });
 
-        app.patch('/porducts/:id', async(req,res)=>{
-            const ID = req.params.id;
-            const updateProduce = req.body;
-            const query = {
-                "_id": new ObjectId(ID)
-            };
-            const update = { $set :
-                {"name": updateProduce.name,
-                "Price": updateProduce.Price}
-            };
-            const option = {};
-            const resuslt = await productCollection.updateOne(query,update,option)
-            res.send(resuslt);
-        })
-        
-        app.get('/porducts', async(req,res)=>{
-            const resuslt = await (await productCollection.find().sort({price_min:1}).toArray())
-            res.send(resuslt);
-        })
+    app.get("/porducts", async (req, res) => {
+      const frojectFild = { title: 1 };
+      const resuslt = await productCollection
+        .find()
+        .sort({ price_min: 1 })
+        .project(frojectFild)
+        .toArray();
+      res.send(resuslt);
+    });
 
-        await client.db('admin').command({ping : 1});
-        console.log("ping your deployment.you successfully connected to MongoDB")
-        
-    } finally {
-        // client.close()
-    }
+    // bids collection
+
+    app.get("/bids", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.buyer_email = email;
+      }
+      const resuslt = await bitsCollection.find(query).toArray();
+      res.send(resuslt);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("ping your deployment.you successfully connected to MongoDB");
+  } finally {
+    // client.close()
+  }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
-}); 
- 
+});
